dc38070e2c4e89b1d52472ce64e6d12448aadd50
K1ngVlad <kingvlad3008@gmail.com>
2023-09-30 23:20:52 +0400

    add icon

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="rus">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.ico" />
         <link
           rel="icon"
           href="/favicon-32x32.png"
